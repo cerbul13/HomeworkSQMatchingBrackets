ed6e5e1a4bf9e2e13c84d423b933725f4efabad2
cerbul13 <cerbadan@yahoo.com>
2022-01-19 13:13:51 +0200

    Add project files.

---
+++ HomeworkSQMatchingBrackets/MatchingBrackets.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkSQMatchingBrackets
+{
+    public class MatchingBrackets
+    {
+        private string text;
+        public string Text
+        {
+            get
+            {
+                return this.text;
+            }
+            set
+            {
+                this.text = value;
+            }
+        }
+        public MatchingBrackets()
+        {
+            Console.Write("Please insert the calculation: ");
+            this.text = Console.ReadLine();
+        }
+        private string TextBetweenBrackets(int p1,int p2)
+        {
+            return text.Substring(p1, p2 - p1 + 1);
+        }
+        public void GetBrackets()
+        {
+            Stack<int> brackets = new Stack<int>();
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i]=='(') 
+                { 
+                    brackets.Push(i);
+                }
+                if (text[i] == ')') 
+                { 
+                    int i1 = brackets.Pop(); 
+                    Console.WriteLine(TextBetweenBrackets(i1,i)); 
+                }
+            }
+        }
+    }
+}
---
+++ HomeworkSQMatchingBrackets/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HomeworkSQMatchingBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MatchingBrackets brackets = new MatchingBrackets();
+            brackets.GetBrackets();
+        }
+    }
+}
